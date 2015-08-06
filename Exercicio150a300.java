fbfbe05ad7ac2dce3ceeb007090f9181978bf683
andersonandrade <andersonandradeti@globomail.com>
2015-08-06 16:41:08 -0300

    imprimir de 150 a 300

---
+++ Exercicio150a300.java
@@ -0,0 +1,10 @@
+
+public class Exercicio150a300 {
+  public static void main(String[] args) {
+	  int valorInicial = 150;
+	  int valorFinal = 300;
+	for (int i = valorInicial; i <= valorFinal; i++) {
+		System.out.println("Valor atual é "+ i);
+	}
+}
+}
