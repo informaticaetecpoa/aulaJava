dc25796322b7fdaefb7f10a6189b42799d3a7d75
andersonandrade <andersonandradeti@globomail.com>
2015-08-06 16:42:09 -0300

    Colacando Comentarios

--- Exercicio150a300.java
+++ Exercicio150a300.java
@@ -1,10 +1,11 @@
 
 public class Exercicio150a300 {
-  public static void main(String[] args) {
-	  int valorInicial = 150;
-	  int valorFinal = 300;
-	for (int i = valorInicial; i <= valorFinal; i++) {
-		System.out.println("Valor atual é "+ i);
+	public static void main(String[] args) {
+		int valorInicial = 150;
+		int valorFinal = 300;
+		// for para fazer a impressão do numeros
+		for (int i = valorInicial; i <= valorFinal; i++) {
+			System.out.println("Valor atual é " + i);
+		}
 	}
-}
 }
